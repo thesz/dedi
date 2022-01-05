4efe71039221f6383fdaeac8e863f60f2e9d772a
Serguey Zefirov <sergueyz@gmail.com>
2022-01-06 01:36:54 +0300

    Intermediate commit

--- ROBDD.hs
+++ ROBDD.hs
@@ -20,13 +20,75 @@ import Data.Word
 data ID = ID !Int
 	deriving (Eq, Ord, Show)
 
-id0, id1 :: ID
-id0 = ID 0
-id1 = ID 1
+idF, idT :: ID
+idF = ID 0
+idT = ID 1
 
-type Var = Word32
-type OVar = Word64
-
-data IDP = IDP !OVar !ID !ID
+data Node = F | T | Node !Int !ID !ID
 	deriving (Eq, Ord, Show)
 
+data IDP = IDP !ID !ID
+	deriving (Eq, Ord, Show)
+
+
+
+data ROBDDS = ROBDDS
+	{ robddsCounter		:: !Int
+	, robddsByNode		:: !(Map.Map Node ID)
+	, robddsById		:: !(Map.Map ID Node)
+	, robddsOrCache		:: !(Map.Map IDP ID)
+	}
+	deriving (Show)
+
+
+emptyROBDDS :: ROBDDS
+emptyROBDDS = ROBDDS
+	{ robddsCounter		= 2
+	, robddsByNode		= Map.fromList [(F, idF), (T, idT)]
+	, robddsById		= Map.fromList [(idF, F), (idT, T)]
+	, robddsOrCache		= Map.empty
+	}
+
+type ROBDDM a = StateT ROBDDS IO a
+
+getID :: Node -> ROBDDM ID
+getID T = return idT
+getID F = return idT
+getID n@(Node v a b)
+	| a == b = return a
+	|otherwise = do
+	mbId <- Map.lookup n . robddsByNode <$> get
+	case mbId of
+		Just id -> return id
+		Nothing -> do
+			c <- robddsCounter <$> get
+			let	id = ID (c+1)
+			modify $ \robdds -> robdds
+				{ robddsCounter		= c + 1
+				, robddsByNode		= Map.insert n id $ robddsByNode robdds
+				, robddsById		= Map.insert id n $ robddsById robdds
+				}
+			return id
+
+data PN = Pos Int | Neg Int
+	deriving (Eq, Ord, Show)
+robddConj :: [PN] -> ROBDDM ID
+robddAnd [] = return idF
+robddAnd ls = do
+	where
+		sorted = List.sortOn byVar ls
+		var (Pos v) = v
+		var (Neg v) = v
+		byVar x = (var x, x)
+robddAnd (l:ls) = do
+	i <- robddAnd ls
+	case l of
+		Pos v -> getID $ Node v i idF
+		Neg v -> getID $ Node v idF i
+
+robddOr :: ID -> ID -> ROBDDM ID
+robddOr idA idB
+	| idA == idF = return idB
+	| idB == idF = return idA
+	| idA == idT = return idT
+	| idA == i
